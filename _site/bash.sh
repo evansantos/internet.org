528037f9aecd36906143ec3f462311ee87c9847b
gpechim <gpechim@gmail.com>
2015-08-08 09:42:34 -0300

    Deploy 20150808_094210

--- _site/bash.sh
+++ _site/bash.sh
@@ -5,15 +5,22 @@
 # Updated At: 2015-08-07
 
 DATE=$(date +"%Y%m%d_%H%M%S")
+HOUR=$(date +"%H%M%S")
+
+# Run production task
 grunt prod
 echo '--------------------------------------------------------------------';
-echo 'Build start at '$DATE'...';
+echo 'Build start at '$HOUR'...';
 echo '--------------------------------------------------------------------';
+
+# Generate packaged and run deploy
 jekyll build
 git add --all
 git commit -m 'Deploy '$DATE''
 git push
 echo '--------------------------------------------------------------------';
-echo 'Build complete!! _site packaged sent to git! WHORRAY!!!';
+echo 'Build complete at '$HOUR'!! _site packaged sent to git! WHORRAY!!!';
 echo '--------------------------------------------------------------------';
+
+# Run developers task
 grunt dev
