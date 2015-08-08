f5f1e034d7bb988e46c1b19f02e46041953d2128
gpechim <gpechim@gmail.com>
2015-08-08 09:34:05 -0300

    Deploy 20150808_093341!

--- _site/bash.sh
+++ _site/bash.sh
@@ -4,14 +4,14 @@
 # Author: Gerson Pechim <gerson.pechim@easytaxi.com.br>, Evandro Santos <evandro.santos@easytaxi.com.br>
 # Updated At: 2015-08-07
 
-now=$(date +"%Y%m%d_%H%M%S")
+DATE=$(date +"%Y%m%d_%H%M%S")
 grunt prod
 echo "--------------------------------------------------------------------";
 echo "Build start...";
 echo "--------------------------------------------------------------------";
 jekyll build
 git add --all
-git commit -m 'Deploy'
+git commit -m 'Deploy '$DATE'!'
 git push
 echo "--------------------------------------------------------------------";
 echo "Build complete!! _site packaged sent to git! WHORRAY!!!";
